12060929c4523f3f40dc5525d33bd78ecac46433
AleLima-K <alexiaxlima@gmail.com>
2022-05-05 19:43:51 -0300

    aaaa

---
+++ app/src/main/java/com/example/appvacinfo/quandoVacinar.kt
@@ -0,0 +1,11 @@
+package com.example.appvacinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class quandoVacinar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quando_vacinar)
+    }
+}
