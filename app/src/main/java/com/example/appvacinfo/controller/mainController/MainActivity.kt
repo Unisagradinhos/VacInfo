419e39b610dd098a22cb4e0d6a10a4c814a92bef
CarlaKremer <carlakremerf@gmail.com>
2022-05-14 21:58:57 -0300

    feat: drawer adicionado na main

--- app/src/main/java/com/example/appvacinfo/controller/mainController/MainActivity.kt
+++ app/src/main/java/com/example/appvacinfo/controller/mainController/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.appvacinfo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toggle : ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_drawer)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
 
         val button_quando_vacinar = findViewById<Button>(R.id.btn_quando_vacinar)
         val button_onde_vacinar = findViewById<Button>(R.id.btn_onde_vacinar)
@@ -17,7 +27,23 @@ class MainActivity : AppCompatActivity() {
         val button_doencas = findViewById<Button>(R.id.btn_doencas)
         val button_sobre = findViewById<Button>(R.id.btn_sobre)
 
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_location -> {
+
+                }
+                R.id.nav_calendar -> {
+
+                }
+            }
+            true
+        }
 
         button_quando_vacinar.setOnClickListener {
             quando_vacinar();
@@ -34,6 +60,13 @@ class MainActivity : AppCompatActivity() {
         button_sobre.setOnClickListener{
             sobre();
         }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun quando_vacinar() {
