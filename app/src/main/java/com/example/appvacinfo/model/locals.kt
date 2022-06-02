af8576cb8f6c6a235d5edd1cfa10a8540118b53c
Guilhermehc94 <guilhermehcustodio3@gmail.com>
2022-06-02 14:47:44 -0300

    C:/Program Files/Git/fix correção dos marks da tela ondevacinar

---
+++ app/src/main/java/com/example/appvacinfo/model/locals.kt
@@ -0,0 +1,11 @@
+package com.example.appvacinfo.model
+
+import com.google.android.gms.maps.model.LatLng
+
+data class Locals(
+    var name: String,
+    var lat: Double,
+    var long:Double,
+    var addres: String
+
+)
