9a9bc5c58917c50cfcc93a778b3535257fe696d4
Guilhermehc94 <guilhermehcustodio3@gmail.com>
2022-06-02 16:24:53 -0300

    feature/ alteração no mark do onde vacinar e icone do apk

---
+++ app/src/main/java/com/example/appvacinfo/model/bitMapHelper.kt
@@ -0,0 +1,31 @@
+package com.example.appvacinfo.model
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
+object BitMapHelper {
+    fun vectorToBitmap(
+        context: Context,
+        @DrawableRes id: Int,
+        @ColorInt color: Int
+        ) : BitmapDescriptor
+        {
+            val vectorDrawable = ResourcesCompat.getDrawable(context.resources, id, null)
+                ?: return BitmapDescriptorFactory.defaultMarker()
+            val bitmap = Bitmap.createBitmap(
+                vectorDrawable.intrinsicWidth,
+                vectorDrawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+            vectorDrawable.draw(canvas)
+            return BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
+}
